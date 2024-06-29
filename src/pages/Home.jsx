7d979689002bdf95c4c7e8b5872c36e9894144a1
Anano Topuria <anano.topuriaa@gmail.com>
2024-06-29 22:46:15 +0400

    style: integrated tailwind css for styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,56 +48,70 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <FaFacebook />
-          <input
-            type="text"
-            name="facebook"
-            placeholder="Facebook"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <FaTwitter />
-          <input
-            type="text"
-            name="twitter"
-            placeholder="Twitter"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <FaLinkedin />
-          <input
-            type="text"
-            name="linkedin"
-            placeholder="LinkedIn"
-            onChange={handleChange}
-          />
-        </div>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="surname"
-          placeholder="Surname"
-          onChange={handleChange}
-        />
-        <input
-          type="file"
-          accept="image/*"
-          name="picture"
-          onChange={handleFileChange}
-        />
-        <button type="submit">Save</button>
-      </form>
+    <div className="flex flex-col gap-2 items-center justify-center h-screen bg-[#415831]">
+      <h1 className="max-w-2xl mx-auto mb-10 text-3xl font-bold leading-normal mt-14 md:text-4xl text-[#f3ebdd] font-dosis">Create Your Own Card</h1>
+      <div className="bg-[#f3ebdd] p-7 mx-6 shadow-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 items-start space-y-6 md:flex-col md:space-y-0 md:space-x-6"
+        >
+          <div className="flex flex-col gap-5">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="w-full px-10 py-3 rounded-full focus:outline-none"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="surname"
+              placeholder="Surname"
+              className="w-full px-10 py-3 rounded-full focus:outline-none"
+              onChange={handleChange}
+            />
+            <input
+              type="file"
+              accept="image/*"
+              name="picture"
+              onChange={handleFileChange}
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="flex gap-3">
+              <FaFacebook color="#415831"/>
+              <input
+                type="text"
+                name="facebook"
+                placeholder="Facebook"
+                className="w-full px-10 py-3 rounded-full focus:outline-none"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex gap-3">
+              <FaTwitter color="#415831"/>
+              <input
+                type="text"
+                name="twitter"
+                placeholder="Twitter"
+                className="w-full px-10 py-3 rounded-full focus:outline-none"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex gap-3">
+              <FaLinkedin color="#415831"/>
+              <input
+                type="text"
+                name="linkedin"
+                placeholder="LinkedIn"
+                className="w-full px-10 py-3 rounded-full focus:outline-none"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <button className="w-full p-3 px-6 rounded-full bg-[#ff6c02] text-white md:w-56 hover:scale-95" type="submit">Save</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,31 +6,54 @@ function User() {
   const user = useSelector((state) => state.user);
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      {user.picture && <img src={user.picture} alt="User" />}
-      <div>
-        <p>{user.name}</p>
-        <p>{user.surname}</p>
+    <div className="flex items-center justify-center h-screen bg-[#ffb400]">
+      <div className="bg-[#ff6c02] p-2 mx-6 rounded-2xl">
+        <div className="flex flex-col md:flex-row rounded-l-xl gap-5">
+          {user.picture && (
+            <img
+              src={user.picture}
+              className="object-fit rounded-xl h-80 md:h-64 md:rounded-l-xl md:rounded-r-none transform hover:scale-105 hover:rounded-xl duration-200"
+              alt="User"
+            />
+          )}
+          <div className="p-6 md:p-12 flex flex-col gap-5">
+            <div className="flex flex-row gap-4 justify-center">
+              <p className="text-xl font-medium text-center text-white md:text-left font-dosis">
+                {user.name}
+              </p>
+              <p className="text-xl font-medium text-center text-white md:text-left font-dosis">
+                {user.surname}
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 justify-center">
+              {user.socialNetworks.facebook && (
+                <div>
+                  <FaFacebook color="#fff9e9" />
+                  <span className="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left">
+                    {user.socialNetworks.facebook}
+                  </span>
+                </div>
+              )}
+              {user.socialNetworks.twitter && (
+                <div>
+                  <FaTwitter color="#fff9e9" />
+                  <span className="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left">
+                    {user.socialNetworks.twitter}
+                  </span>
+                </div>
+              )}
+              {user.socialNetworks.linkedin && (
+                <div>
+                  <FaLinkedin color="#fff9e9" />
+                  <span className="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left">
+                    {user.socialNetworks.linkedin}
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-      {user.socialNetworks.facebook && (
-        <div>
-          <FaFacebook />
-          <span>{user.socialNetworks.facebook}</span>
-        </div>
-      )}
-      {user.socialNetworks.twitter && (
-        <div>
-          <FaTwitter />
-          <span>{user.socialNetworks.twitter}</span>
-        </div>
-      )}
-      {user.socialNetworks.linkedin && (
-        <div>
-          <FaLinkedin />
-          <span>{user.socialNetworks.linkedin}</span>
-        </div>
-      )}
     </div>
   );
 }
